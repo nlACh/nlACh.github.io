2206d076908a53e518632e2cb7f40abb6eace56c
Nilayan Paul <nlpl931@gmail.com>
2025-05-16 23:26:34 +0530

    fixed indexing

--- etc/collections/index.js
+++ etc/collections/index.js
@@ -3,4 +3,4 @@ export const feed = i => i.getFilteredByGlob("./src/content/pub_type/*.md");//.r
 
 export const conf_id = i => i.getFilteredByGlob("./src/content/conf_id/*.md").reverse();
 export const preprint_id = i => i.getFilteredByGlob("./src/content/preprint_id/*.md").reverse();
-export const publication_id = i => i.getFilteredByGlob("./src/content/publication_id/*.md").reverse();
+export const publication_id = i => i.getFilteredByGlob("./src/content/journal_id/*.md").reverse();
